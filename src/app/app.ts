e682170b9c0f76236eb07a345d29f45aa1f40998
Amr-Saied <2021170375@cis.asu.edu.eg>
2025-07-10 15:33:27 +0300

    Add Bootstrap and Font Awesome, enable SSL for development server, and implement user fetching in app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected title = 'AngularChattingAppFrontEnd';
+export class App implements OnInit {
+  protected title = 'chatting app';
+  protected Users: any;
+  protected usersUrlHttp = 'http://localhost:5194/Users/GetUsers';
+  protected usersUrlHttps = 'https://localhost:7095/Users/GetUsers';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const usersUrl =
+      window.location.protocol === 'https:'
+        ? this.usersUrlHttps
+        : this.usersUrlHttp;
+    this.http.get(usersUrl).subscribe({
+      next: (response) => {
+        this.Users = response;
+        console.log(this.Users);
+      },
+      error: (error) => {
+        console.error('Error fetching users from the backend', error);
+      },
+    });
+  }
 }
