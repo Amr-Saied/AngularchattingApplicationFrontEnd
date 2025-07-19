b5cbfb512f6ac5c8ddd6bba2db5ff0b6389d1835
Amr-Saied <2021170375@cis.asu.edu.eg>
2025-07-20 01:51:35 +0300

    AddedTempLocalStorageForPersistantLogin&&AddedInitialHomePage&&ResigerationForm

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { User } from './user';
+import { User } from '../_models/user';
 import { ChangeDetectorRef } from '@angular/core';
 import { Nav } from '../nav/nav';
+import { Account } from '../_services/account';
+import { Home } from '../home/home';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
   standalone: true,
-  imports: [CommonModule, Nav],
+  imports: [CommonModule, Nav, Home, RouterModule],
 })
 export class App implements OnInit {
   public loggedIn = false;
-  protected title = 'chatting app';
+  // protected title = 'chatting app';
   protected Users: User[] = [];
   protected usersUrlHttp = 'http://localhost:5194/Users/GetUsers';
   protected usersUrlHttps = 'https://localhost:7095/Users/GetUsers';
 
-  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
+  constructor(
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef,
+    private account: Account
+  ) {}
 
   ngOnInit() {
+    // Check if user is already logged in from local storage
+    this.loggedIn = this.account.isLoggedIn();
+
     const usersUrl =
       window.location.protocol === 'https:'
         ? this.usersUrlHttps
--- src/nav/nav.ts
+++ src/nav/nav.ts
@@ -1,8 +1,9 @@
 // nav.ts
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { Account } from '../_services/account';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { LoggedUser } from '../_models/logged-user';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./nav.css'],
   imports: [CommonModule, FormsModule],
 })
-export class Nav {
+export class Nav implements OnInit {
   model: any = {};
   loggedIn = false;
 
@@ -19,11 +20,29 @@ export class Nav {
 
   constructor(private account: Account) {}
 
+  ngOnInit() {
+    // Check if user is already logged in from local storage
+    this.loggedIn = this.account.isLoggedIn();
+    if (this.loggedIn) {
+      this.loggedInChange.emit(true);
+    }
+  }
+
   login() {
     this.account.login(this.model).subscribe({
-      next: (response) => {
+      next: (response: any) => {
         console.log(response);
         this.loggedIn = true;
+
+        // Save logged user data to local storage
+        if (response.username && response.token) {
+          const loggedUser: LoggedUser = {
+            username: response.username,
+            token: response.token,
+          };
+          this.account.saveLoggedUserToStorage(loggedUser);
+        }
+
         this.loggedInChange.emit(true); // Notify parent
       },
       error: (error) => {
@@ -34,6 +53,8 @@ export class Nav {
 
   logout() {
     this.loggedIn = false;
+    // Clear logged user data from local storage
+    this.account.clearLoggedUserFromStorage();
     this.loggedInChange.emit(false); // Notify parent
   }
 }
